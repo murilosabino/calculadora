c2fb428d01cda9120e21a2082e3fdad988f143d5
Murilo <murilosabino11@gmail.com>
2021-02-03 20:32:28 -0300

    atualizando soma

--- src/Operacoes.java
+++ src/Operacoes.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Operacoes {
 
@@ -9,17 +10,19 @@ public class Operacoes {
     public void soma() {
         boolean continua = true;
         while (continua) {
-            List<Double> numeros = new LinkedList<>();
+            ArrayList<Double> numeros = new ArrayList<>();
             System.out.println("Digite o número ou 0 para sair: \n");
             Scanner soma1 = new Scanner(System.in);
             double n = soma1.nextDouble();
             if(n == 0){
                 continua = false;
+                System.out.println(numeros);
                 System.out.println("Fim do programa");
             }
             else{
             numeros.add(n);
-            System.out.println(numeros);
+            
+            
             }
 
 
